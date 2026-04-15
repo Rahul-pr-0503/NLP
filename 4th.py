495c5d3d681c8a3837435defcd738fdea8e4d1e3
Rahul S Tawarakhed <rahultawarakhed@gmail.com>
2026-04-15 09:59:54 +0530

    Add files via upload

--- 4th.py
+++ 4th.py
@@ -1,1 +1,22 @@
-
+import nltk
+from nltk import CFG
+from nltk.parse import RecursiveDescentParser, ShiftReduceParser
+grammar = CFG.fromstring("""
+S -> NP VP | S NP
+NP -> Det N | N
+VP -> V NP | V
+Det -> 'the' | 'a'
+N -> 'cat' | 'dog' | 'rat'
+V -> 'chases' | 'eats'
+""")
+tokens = ['the', 'cat', 'chases', 'a', 'rat']
+rd_parser = RecursiveDescentParser(grammar)
+print("\nTop-Down Parsing (Recursive Descent):")
+for tree in rd_parser.parse(tokens):
+    print(tree)
+    tree.pretty_print()
+sr_parser = ShiftReduceParser(grammar)
+print("\nBottom-Up Parsing (Shift-Reduce):")
+for tree in sr_parser.parse(tokens):
+    print(tree)
+    tree.pretty_print()
